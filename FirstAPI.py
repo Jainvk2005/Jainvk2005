2ffee3a0fe5192b3d2d2f73a54abd742a3d59ea3
Jainvk2005 <163175125+Jainvk2005@users.noreply.github.com>
2024-03-12 23:43:58 +0530

    Update FirstAPI.py

--- FirstAPI.py
+++ FirstAPI.py
@@ -1,1 +1,16 @@
 ##first API
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "This is the route."
+    return name
+
+@app.route('/hello')
+def hello():
+    name = "Let us Reach 200 CEs together"
+    return name
+
+if __name__ == "__main__":
+    app.run()
